b2711ccc7da56ab655440822ba3d2316a66167d5
ericasinds <erica@eleerealty.com>
2026-05-17 18:17:01 -0400

    Create themeParts.js

---
+++ src/styles/themeParts.js
@@ -0,0 +1,9 @@
+import { theme1 } from "./theme1.js";
+import { theme2 } from "./theme2.js";
+import { theme3 } from "./theme3.js";
+import { theme4 } from "./theme4.js";
+import { theme5 } from "./theme5.js";
+import { theme6 } from "./theme6.js";
+import { theme7 } from "./theme7.js";
+
+export const themeCss = [theme1, theme2, theme3, theme4, theme5, theme6, theme7].join("");
